bf13f00dbcc39130ce70d92c605d9731902f329d
Go Hayakawa <go.hayakawa@gmail.com>
2015-07-02 02:28:03 +0900

    Create xcode project

---
+++ project/SwiftNeuralNetwork/SwiftNeuralNetwork.h
@@ -0,0 +1,19 @@
+//
+//  SwiftNeuralNetwork.h
+//  SwiftNeuralNetwork
+//
+//  Created by Go Hayakawa on 2015/07/02.
+//  Copyright (c) 2015年 Go Hayakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftNeuralNetwork.
+FOUNDATION_EXPORT double SwiftNeuralNetworkVersionNumber;
+
+//! Project version string for SwiftNeuralNetwork.
+FOUNDATION_EXPORT const unsigned char SwiftNeuralNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNeuralNetwork/PublicHeader.h>
+
+
