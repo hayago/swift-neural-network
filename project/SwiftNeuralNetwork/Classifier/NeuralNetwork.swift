dbd4094b4f0d728c81a99f0369eb9076fd7a20d5
Go Hayakawa <go.hayakawa@gmail.com>
2015-07-05 17:38:55 +0900

    Create NewralNetwork.swift

---
+++ project/SwiftNeuralNetwork/Classifier/NeuralNetwork.swift
@@ -0,0 +1,9 @@
+//
+//  NeuralNetwork.swift
+//  SwiftNeuralNetwork
+//
+//  Created by Go Hayakawa on 2015/07/05.
+//  Copyright (c) 2015年 Go Hayakawa. All rights reserved.
+//
+
+import Foundation
---
+++ project/SwiftNeuralNetworkTests/ClassifierTest/NeuralNetworkTests.swift
@@ -0,0 +1,22 @@
+//
+//  NeuralNetworkTests.swift
+//  NeuralNetworkTests
+//
+//  Created by Go Hayakawa on 2015/07/02.
+//  Copyright (c) 2015年 Go Hayakawa. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class NeuralNetworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
