bad96c5a02dcf05fb6587583ee64b0b3d6e56076
Go Hayakawa <go.hayakawa@gmail.com>
2015-07-26 18:05:50 +0900

    Modify NeuralNetworkModelTests.swift

--- project/SwiftNeuralNetworkTests/ModelTest/NeuralNetworkModelTests.swift
+++ project/SwiftNeuralNetworkTests/ModelTest/NeuralNetworkModelTests.swift
@@ -11,12 +11,121 @@ import XCTest
 
 class NeuralNetworkModelTests: XCTestCase {
     
+    var inputs: [[Double]] = []
+    var targets: [[Double]] = []
+    
+    
     override func setUp() {
         super.setUp()
+        
+        inputs.removeAll(keepCapacity: false)
+        targets.removeAll(keepCapacity: false)
+        
+        if let resourcePath = NSBundle(forClass: self.dynamicType).pathForResource("irisdata", ofType: "csv") {
+            if let resource = String(contentsOfFile: resourcePath) {
+            
+                let featureSize = 4
+                let labels = ["Iris-setosa", "Iris-versicolor", "Iris-virginica"]
+                
+                var lines: [String] = []
+                resource.enumerateLines { (line, stop) -> () in
+                    lines.append(line)
+                }
+       
+                // Shuffle
+                for i in 0..<(lines.count - 1) {
+                    let j = Int(arc4random_uniform(UInt32(lines.count - i))) + i
+                    swap(&lines[i], &lines[j])
+                }
+
+                var data: [[Double]] = [[Double]](count: 4, repeatedValue: [])
+
+                for (index, line) in enumerate(lines) {
+                    let elements = line.componentsSeparatedByString(",")
+                    
+                    for i in 0..<featureSize {
+                        data[i].append(NSString(string: elements[i]).doubleValue)
+                    }
+                    
+                    var target: [Double] = [0.0, 0.0, 0.0]
+                    if let i = find(labels, elements[featureSize]) {
+                        target[i] = 1.0
+                    }
+                    targets.append(target)
+                }
+                
+                
+                let standardizationData = data.map { Math.standardizate($0) }
+                
+                var features: [[Double]] = [[Double]](count: standardizationData.first!.count, repeatedValue: [])
+                for d in standardizationData {
+                    for (index, v) in enumerate(d) {
+                        features[index].append(v)
+                    }
+                }
+                
+                inputs = features
+                
+            
+            } else {
+                XCTFail("Failed to parse test data")
+            }
+        
+        } else {
+            XCTFail("Failed to load test data")
+        }
+
     }
+
     
-    override func tearDown() {
-        super.tearDown()
+    func testPredict() {
+
+        let model = NeuralNetworkModel(inputSize: 4, hiddenSize: 10, outputSize: 3, learningRate: 0.05)
+       
+        let testDataInterval = 3
+      
+        // Training
+        for (index, input) in enumerate(inputs) {
+            
+            if (index % testDataInterval == 0) {
+                continue
+            }
+            
+            model.fit(input, targets: targets[index])
+        }
+        
+        // Testing
+        var totalCount = 0
+        var correctCount = 0
+        for (index, input) in enumerate(inputs) {
+            
+            if (index % testDataInterval != 0) {
+                continue
+            }
+            
+            let output = model.predict(input)
+           
+            
+            var label = 0
+            var max = output[0]
+            for (i, v) in enumerate(output) {
+                if (v > max) {
+                    label = i
+                    max = v
+                }
+            }
+            
+            println("\(output)---\(targets[index])---\(label)")
+
+            
+            if (targets[index][label] > 0) {
+                correctCount++
+            }
+            
+            totalCount++
+        }
+        
+        println("Accuracy: \(Double(correctCount) / Double(totalCount))")
     }
-    
+
 }
